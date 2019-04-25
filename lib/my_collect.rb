736d247a41c8cd4f602951893ff62f0a10c03486
Caleb Aviles <earthctzn1@gmail.com>
2019-04-25 01:06:44 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   index = 0 
   new_collection = []
   while index < collection.length 
-  new_collection << yield(collection)
+  new_collection << yield(collection[index])
   index += 1
   end
   new_collection
